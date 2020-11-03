aee48efaf3881fae2087c75e4a6c846bdd87048d
Your Name <you@example.com>
2020-11-03 09:30:51 -0600

    init commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ClassesExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Mustang";
+            myCar.Year = 1985;
+
+            Console.WriteLine($"{myCar.Make} {myCar.Model} {myCar.Year}");
+        }
+    }
+}
